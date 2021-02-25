1c8ba9f892cd3293f03558cdab079a9e9bbb52bc
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-02-25 13:28:01 +0700

    Add files via upload

---
+++ Swift Fundamentals/8 kyu/Grasshopper - Personalized Message.swift
@@ -0,0 +1,14 @@
+/* Personalized greeting
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	            | return
+----------------------------------------
+name equals owner	| 'Hello boss'
+otherwise	        | 'Hello guest'
+*/
+
+func great(_ name: String, _ owner: String) -> String {
+    return name == owner ? "Hello boss" : "Hello guest"
+}
