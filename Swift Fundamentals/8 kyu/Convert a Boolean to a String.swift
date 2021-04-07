2c3c119c0b17d79d65525e3ab52e7b11d4ecf943
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-04-08 02:47:29 +0700

    Add files via upload

---
+++ Swift Fundamentals/8 kyu/Convert a Boolean to a String.swift
@@ -0,0 +1,7 @@
+/* Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given. 
+*/
+
+func booleanToString(_ b: Bool) -> String {
+    return String(b)
+}
