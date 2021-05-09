f859d1ea6d2f4156bff07f5b434dd9cfd6607382
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-05-09 21:05:40 +0700

    Add files via upload

---
+++ C++ Fundamentals/8 kyu/Sum of positive.cpp
@@ -0,0 +1,20 @@
+/* You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+#include <vector>
+
+int positive_sum (const std::vector<int> arr){
+if (arr.empty()) {return 0;} else {
+    int sum = 0;
+    for (auto & element : arr) {
+      if (element > 0) {
+        sum += element;
+      }
+    }
+    return sum;
+  }
+}
