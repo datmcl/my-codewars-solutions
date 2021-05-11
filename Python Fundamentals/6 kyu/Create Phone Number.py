cfbaa785704261966b583e5caabfaf79331dc11a
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-05-11 21:44:14 +0700

    Update Create Phone Number.py

--- Python Fundamentals/6 kyu/Create Phone Number.py
+++ Python Fundamentals/6 kyu/Create Phone Number.py
@@ -10,4 +10,4 @@ Don't forget the space after the closing parentheses!
 """
 
 def create_phone_number(n):
-    return ''.join(f'({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}')
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
